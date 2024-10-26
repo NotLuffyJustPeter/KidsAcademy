e05f2d97b519dba228f1c6c0d3128334d41639e1
NotLuffyJustPeter <pedroromangd@gmail.com>
2024-10-26 15:06:48 -0600

    Pantalla Inicio
    Se agrego el layout completo de pantalla inicio, para seleccionar los modulos y se manejo la logica de los intents para nuevas pantallas

--- app/src/main/java/hrn/prgd/kidsacademy/PantallaInicio.kt
+++ app/src/main/java/hrn/prgd/kidsacademy/PantallaInicio.kt
@@ -1,4 +1,33 @@
 package hrn.prgd.kidsacademy
 
-class PantallaInicio {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class PantallaInicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pantalla_inicio)
+    }
+
+    fun onSpanishClick(view: View) {
+        val intent = Intent(this, Spanish::class.java)
+        startActivity(intent)
+    }
+
+    fun onMathClick(view: View) {
+        val intent = Intent(this, Math::class.java)
+        startActivity(intent)
+    }
+
+    fun onEthicsClick(view: View) {
+        val intent = Intent(this, Etica::class.java)
+        startActivity(intent)
+    }
+
+    fun onScienceClick(view: View) {
+        val intent = Intent(this, Ciencias::class.java)
+        startActivity(intent)
+    }
 }
