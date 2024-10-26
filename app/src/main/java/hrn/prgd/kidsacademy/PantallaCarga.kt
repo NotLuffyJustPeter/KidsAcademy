06c99b130ab14c42236c96ddf4d6f45b4710bb23
NotLuffyJustPeter <pedroromangd@gmail.com>
2024-10-26 17:49:02 -0600

    Update PantallaCarga
    Se agrego la funcion para Pantalla de carga, asi como su layout

--- app/src/main/java/hrn/prgd/kidsacademy/PantallaCarga.kt
+++ app/src/main/java/hrn/prgd/kidsacademy/PantallaCarga.kt
@@ -1,4 +1,18 @@
 package hrn.prgd.kidsacademy
 
-class PantallaCarga {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class PantallaCarga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pantalla_carga)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
 }
