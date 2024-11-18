aabe3f32f73985305167cead15339d49293b9153
OmiHernandez <hernandez.romo.n14@gmail.com>
2024-11-18 00:18:09 -0600

    android manifest modificado

---
+++ app/src/main/java/hrn/prgd/kidsacademy/Actividad4Math.kt
@@ -0,0 +1,11 @@
+package hrn.prgd.kidsacademy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Actividad4Math : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actividad1_math)
+    }
+}
